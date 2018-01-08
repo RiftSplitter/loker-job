02cc1f8d42e4b95bdb5021694eb7e5399e8d0fe9
more_one_night <williamcandra56@gmail.com>
2018-01-08 19:00:20 +0700

    First commit

---
+++ database/migrations/2018_01_07_134458_create_jobs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('job_id');
+            $table->string('job_name');
+            $table->integer('company_id')->unsigned()->index();
+            $table->timestamps();
+
+            $table->foreign('company_id')->references('company_id')->on('companies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jobs');
+    }
+}
